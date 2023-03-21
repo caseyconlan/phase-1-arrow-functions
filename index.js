7d998bae8664f2b2379b49c7f4fb0d7f6697a43f
root <root@DESKTOP-1IHRCNC>
2023-03-21 12:32:45 -0700

    6 passing tests

---
+++ index.js
@@ -0,0 +1,8 @@
+function divide() {
+    return 2000/100
+}
+const square = (parameter1) => parameter1 * parameter1;
+square(5); //=> 25
+
+const add = (parameter1, parameter2) => parameter1 + parameter2;
+add(2,3); //=> 5
